35b569269e0e92203fafa678e50d6883eba63bf5
slevine3 <levinesam3@gmail.com>
2022-01-21 19:08:17 +0200

    travelpage

--- public/script.js
+++ public/script.js
@@ -3,11 +3,6 @@
 let body = document.body
 let userdata = document.querySelector('.userdata')
 let button = document.querySelector('.submit')
-// let button = document.createElement('button')
-// let textNode1 = document.createTextNode('Look up Weather')
-// button.classList.add('submit')
-// button.appendChild(textNode1)
-// body.appendChild(button)
 button.addEventListener('click', lookupWeather)
 let div = document.querySelector('.div')
 
@@ -23,26 +18,35 @@ async function weatherApi(){
 	//This function fetches the weather api and appends to the div (class div)
 	try {
 		const results = await fetch(`http://api.weatherapi.com/v1/current.json?key=1529a6c2d12742cea19173139220601&q=${userdata.value}`)
+		const teleport = await fetch(`https://api.teleport.org/api/urban_areas/slug:${userdata.value}/images/`)
+		const teleport2 = await fetch(`https://api.teleport.org/api/urban_areas/slug:${userdata.value}/scores/`)
 		if (results.status !== 200){
 			div.innerText = 'Please make sure city spelling is correct'
 			throw new Error('Error, results are not 200')
 		}else {
 			let weatherResults = await results.json();
-
+			let teleportResults = await teleport.json();
+			let teleportResults2 = await teleport2.json();
+			console.log(teleportResults2)
 
 	//This section appends the weatherResults to the div inner text
-	// let div = document.getElementById("info");
-	// let icon = document.getElementById("icon");
-	// let src = weatherResults.current.condition.icon;
-	// let img = `<img src=${src}></img>`;
-	// icon.innerHTML = img;
+	let icon = document.createElement('icon')
+	let src = weatherResults.current.condition.icon;
+	let https = 'https:'
+	let final = https.concat(src);
+	let img = `<img src=${final}></img>`;
 
+	let banner = document.querySelector('.banner')
+	let pic = teleportResults.photos[0].image.web;
+	let finalPic = `<img src=${pic}></img>`;
 
-	div.innerText = 
+	banner.innerHTML = `${finalPic}`
+
+	div.innerHTML = 
 	(`${weatherResults.location.name}, ${weatherResults.location.region} 
 		Current Temperature Farenheit: ${weatherResults.current.temp_f} 
 		Description: ${weatherResults.current.condition.text} 
-		Humidiy: ${weatherResults.current.humidity}`)
+		Humidiy: ${weatherResults.current.humidity} ${img}`)
 
 		//This section adds background images depending on current weather temperature
 
